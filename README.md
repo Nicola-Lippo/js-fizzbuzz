ad0d90157c93b269a80b0231f42801cea8e0fc57
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-02 10:42:56 +0200

    [UPD]correzione consegna

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 1 Scrivo un programma che stampi in console i numeri da 1 a 100
 
-2 sostituisco i numeri multipli di 3 con Fizz
+2 sostituisco i numeri multipli di 15 con FizzBuzz
 
-3 sostituisco i numeri multipli di 5 con Buzz
+3 sostituisco i numeri multipli di 3 con Fizz
 
-4 sostituisco i numeri multipli sia di 3 che di  5 con FizzBuzz
+4 sostituisco i numeri multipli di 5 con Buzz
--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
+const ul = document.querySelector('ul.list');
+
 //Scrivo un programma che stampi in console i numeri da 1 a 100
 for (let number = 1; number <= 100; number++) {
+    const li = document.createElement('li');
+    //sostituisco i numeri multipli di 15 con FizzBuzz
+    if (number % 15 == 0) {
+        console.log ("FizzBuzz");
+    }
     //sostituisco i numeri multipli di 3 con Fizz
-    if (number % 3 == 0) {
+    else if (number % 3 == 0) {
         console.log ("Fizz");
     }
-    //sostituisco i numeri multipli di 5 con Buzz
+    //sostituisco i numeri multipli  5 con Buzz
     else if (number % 5 == 0) {
         console.log ("Buzz");
-    }
-    //sostituisco i numeri multipli sia di 3 che di  5 con FizzBuzz
-    else if (number % 3 == 0 && number % 5 == 0) {
-        console.log ("FizzBuzz");
     }
     //numero normale visibile in console
     else {
